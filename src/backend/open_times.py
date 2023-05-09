29d85c8040bdb3586a2abc08d9fb1809b773d480
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-09 16:54:33 +0100

    just back up draft of open times parsing

---
+++ src/backend/open_times.py
@@ -0,0 +1,54 @@
+# %%
+import pandas as pd
+import re
+
+# %%
+Days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+Days = Days + [x + "s" for x in Days]
+DAYS = [x.upper() for x in Days]
+days = [x.lower() for x in Days]
+weekdays = Days + DAYS + days
+
+
+# %%
+def parse_opening_str(text, id_name):
+    row_list = []
+    result = re.search(
+        f"({'|'.join(weekdays)})"
+        + r" *([0-9\.:]+) *[-–]? *([0-9\.:]+)\n?(.*)",  # noqa:W605
+        text,
+    )
+    if result:
+        if len(result.groups()) > 3:
+            row_list = parse_opening_str(result.groups()[3], id_name)
+        row_list.append(
+            {
+                "id": id_name,
+                "day": result.groups()[0],
+                "start": result.groups()[1],
+                "end": result.groups()[2],
+            }
+        )
+    return row_list
+
+
+# %%
+def parse_open_times(df, col_in="Opening", col_id="Name"):
+    df_out = df.apply(
+        lambda x: pd.DataFrame(parse_opening_str(x[col_in], x[col_id])), axis=1
+    ).to_list()
+
+    return pd.concat(df_out)
+
+
+# %%
+df = pd.read_csv("../../data/foodbanks.csv")
+parse_open_times(df)
