0d28db2e6490351ca8a13e5762ac4015ef6fd7c6
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-10 14:50:44 +0100

    postcode input

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    pc_list = ["S10 2TN", "S10 2RE", "S10 2TN"]
+    range = request.form["travel_range"]
+    range = range + 1
+    return render_template("index.html", pc_list=pc_list)
 
 
 if __name__ == "__main__":
