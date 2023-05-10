174936240722d8883d13dfa5897b4e97e0ed6e37
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-10 20:52:40 +0100

    postcode check and start on table

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,13 +1,44 @@
-from flask import Flask, render_template  # , request
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
+pc_list = ["S10 2TN", "S10 2RE", "S10 2GR"]
+foodbanks = pd.DataFrame(
+    [
+        ["London", 51.5072, -0.1275],
+        ["Birmingham", 52.4800, -1.9025],
+        ["Manchester", 53.4794, -2.2453],
+        ["Liverpool", 53.4075, -2.9919],
+        ["Portsmouth", 50.8058, -1.0872],
+        ["Southampton", 50.9025, -1.4042],
+        ["Nottingham", 52.9533, -1.1500],
+    ],
+    columns=["Name", "Long", "Lat"],
+)
 
 
-@app.route("/", methods=["GET", "POST"])
+# %%
+# %%
+@app.route("/")
 def index():
-    pc_list = ["S10 2TN", "S10 2RE", "S10 2TN"]
-    # range = request.form["travel_range"]
-    return render_template("index.html", pc_list=pc_list)
+    return render_template("index.html", pc_list=pc_list, foodbanks=None)
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    query_type = request.form["query_type"]
+    postcode = request.form["pcode"]
+    range = request.form["range_val"]
+    days = {
+        x: (1 if x in request.form.keys() else 0)
+        for x in ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    }
+    # foodbanks = query_backend( above parameters)
+    print(f"{query_type, days, postcode, range }")
+    # return f"{query_type, days, postcode, range }"
+    return render_template(
+        "index.html", pc_list=pc_list, foodbanks=foodbanks.to_html(classes="data")
+    )
 
 
 if __name__ == "__main__":
