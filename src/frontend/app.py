f8c545cdd90dc35cc8aca55ee64654f5e231ffe8
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-10 16:49:36 +0100

    range slider update

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template  # , request
 
 app = Flask(__name__)
 
@@ -6,8 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     pc_list = ["S10 2TN", "S10 2RE", "S10 2TN"]
-    range = request.form["travel_range"]
-    range = range + 1
+    # range = request.form["travel_range"]
     return render_template("index.html", pc_list=pc_list)
 
 
