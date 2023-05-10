795d9874a52dc530ada2abde56b1e2adb0968b75
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-10 14:09:06 +0100

    layout experiment

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/map', methods=['POST'])
-def map():
-    lat = request.form['lat']
-    lng = request.form['lng']
-    return render_template('map.html', lat=lat, lng=lng)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
