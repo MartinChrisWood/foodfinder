684b9105bbbd964b78cbc1ad1a0c1947300f0215
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-10 11:54:12 +0100

    small improvement to test

--- tests/test_open_times.py
+++ tests/test_open_times.py
@@ -11,21 +11,30 @@ from src.backend.open_times import (
 )
 
 open_str = "Tuesday 11.00 – 13.00\nFriday 11.00 – 13.00"
-open_df = pd.DataFrame({"Name": ["Hillsborough"], "Opening": [open_str]})
+open_df = pd.DataFrame(
+    {
+        "Name": ["Hillsborough", "Crookes"],
+        "Opening": [open_str, "Tuesday 14.00 – 15.00"],
+    }
+)
 out_str = [
     {"id": "bla", "day": "Friday", "start": "11.00", "end": "13.00"},
     {"id": "bla", "day": "Tuesday", "start": "11.00", "end": "13.00"},
 ]
 out_df = pd.DataFrame(
     {
-        "id": ["Hillsborough", "Hillsborough"],
-        "day": ["Friday", "Tuesday"],
-        "start": ["11.00", "11.00"],
-        "end": ["13.00", "13.00"],
+        "id": ["Hillsborough", "Hillsborough", "Crookes"],
+        "day": ["Friday", "Tuesday", "Tuesday"],
+        "start": ["11.00", "11.00", "14.00"],
+        "end": ["13.00", "13.00", "15.00"],
     }
 )
 time_to_open = pd.Series(
-    [pd.Timedelta("1 days 01:00:00"), pd.Timedelta("4 days 01:00:00")],
+    [
+        pd.Timedelta("0 days 00:00:00"),
+        pd.Timedelta("0 days 01:30:00"),
+        pd.Timedelta("2 days 22:30:00"),
+    ],
     name="time_to_open",
 )
 
@@ -44,10 +53,9 @@ def test_parse_open_df():
 
 
 def test_sort_by_time():
-    time_now = datetime(2023, 5, 8, 10, 0, 0, 0)
+    time_now = datetime(2023, 5, 9, 12, 30, 0, 0)
     test_df = sort_by_time_to_open(out_df, time_now)
     pd.testing.assert_series_equal(
         test_df.time_to_open,
         time_to_open,
-        check_index=False,
     )
