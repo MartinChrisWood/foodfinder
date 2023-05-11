1328ad7a35ed7baf05ee7c7c6dd0abf459e7f61a
Edward-Jackson-ONS <edward.jackson@ons.gov.uk>
2023-05-11 17:05:17 +0100

    Simple unit tests for foodfind_asap function

---
+++ tests/test_frontend_handler.py
@@ -0,0 +1,26 @@
+"""Test foodfind_asap function in frontend_handler.py"""
+
+from datetime import datetime
+import pytest
+from src.backend.frontend_handler import foodfind_asap
+
+# test for closest foodbank ID given date and starting postcode
+mock_data = [
+    {"timestamp": datetime(2023, 5, 11, 12, 10, 23, 442020), "postcode": "S1 1AA", "nearest_on_date": 13},
+    {"timestamp": datetime(2023, 6, 11, 12, 10, 23, 442020), "postcode": "S1 1AA", "nearest_on_date": 3},
+    {"timestamp": datetime(2023, 5, 11, 12, 10, 23, 442020), "postcode": "S9 1EA", "nearest_on_date": 15},
+    # same closest as above as Tuesday next availability from Sunday
+    {"timestamp": datetime(2023, 6, 11, 12, 10, 23, 442020), "postcode": "S9 1EA", "nearest_on_date": 15},
+
+]
+
+@pytest.mark.parametrize("config", mock_data)
+def test_foodfind_asap(config):
+    """Test filtering to closest foodbank by distance and availability"""
+
+    found = foodfind_asap(method="postcode",
+                          time_stamp=config["timestamp"],
+                          postcode=config["postcode"])
+    # assert that top hit is correct
+    assert found.loc[0, "ID"] == config["nearest_on_date"]
+
