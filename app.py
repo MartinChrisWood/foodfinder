27970eea683237967329f8a711562a0fe86efdf7
ivyONS <26869198+ivyONS@users.noreply.github.com>
2023-05-12 08:56:16 +0100

    centre map on search start and add marker

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from shapely.geometry import Point
 import re
 
+MAP_CENTRE = [53.4, -1.4]
+MAP_ZOOM = 11
 pc_data = pd.read_csv("data/shef_pc_coords_lookup.csv")
 pc_list = pc_data["postcode"].to_list()
 
@@ -39,7 +41,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template(
-        "index.html", pc_list=pc_list, foodbanks="", df=pd.DataFrame()
+        "index.html",
+        pc_list=pc_list,
+        foodbanks="",
+        df=pd.DataFrame(),
+        map_centre=MAP_CENTRE,
+        map_zoom=MAP_ZOOM,
+        marker=[0, 0],
     )
 
 
@@ -47,16 +55,31 @@ def index():
 def search():
     query_type = request.form["query_type"]
     query_location = request.form["query_location"]
-    postcode = request.form["pcode"]
-    coords = re.search(
-        r"LatLng\(([0-9\.-]+), ([0-9§.-]+)\)", request.form["coords"]  # noqa:W605
-    )
-    if coords:
-        gr = coords.groups()
-        coords = Point(float(gr[1]), float(gr[0]))
+    map_centre = MAP_CENTRE
+    map_zoom = MAP_ZOOM
+    marker = [0, 0]
+    if query_location == "postcode":
+        postcode = request.form["pcode"]
+        if postcode in pc_list:
+            ind = pc_list.index(postcode)
+            marker = [pc_data.lat[ind], pc_data.long[ind]]
+            map_centre = marker
+            map_zoom = MAP_ZOOM + 2
+
     else:
-        coords = Point(0, 0)
-    # TODO check valid location input
+        coords = re.search(
+            r"LatLng\(([0-9\.-]+), ([0-9§.-]+)\)", request.form["coords"]  # noqa:W605
+        )
+        if coords:
+            gr = coords.groups()
+            coords = Point(float(gr[1]), float(gr[0]))
+            marker = [float(gr[0]), float(gr[1])]
+            map_centre = marker
+            map_zoom = MAP_ZOOM + 2
+        else:
+            coords = Point(0, 0)
+
+    # TODO check valid location input and display error?
 
     range = float(request.form["range_val"])
     days = {
@@ -107,6 +130,9 @@ def search():
         pc_list=pc_list,
         foodbanks=html_table(foodbanks),
         df=foodbanks,
+        map_centre=map_centre,
+        map_zoom=map_zoom,
+        marker=marker,
     )
 
 
